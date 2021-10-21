8571bb4b4ad11f7a8faeda2956bf9434fa14a072
oluwatosinolamilekan <oluwatosinolamilekan@gmail.com>
2021-10-21 21:05:12 +0100

    add error log

--- app/Http/Controllers/AdvertiseCampaignController.php
+++ app/Http/Controllers/AdvertiseCampaignController.php
@@ -11,6 +11,7 @@ use App\Services\ListsCampaigns;
 use App\Services\ShowCampaign;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class AdvertiseCampaignController extends Controller
 {
@@ -26,6 +27,7 @@ class AdvertiseCampaignController extends Controller
             $campaign = (new CreateCampaign())->run($request);
             return new CampaignResource($campaign);
         }catch (Exception $exception){
+            Log::error("Create Campaign error : {$exception->getMessage()}");
             return response()->json(DisplayResponse::reFailed($exception->getMessage()),500);
         }
     }
@@ -36,6 +38,7 @@ class AdvertiseCampaignController extends Controller
             $campaign = (new ShowCampaign())->run($id);
             return new CampaignResource($campaign);
         }catch (Exception $exception){
+            Log::error("Show Campaign error : {$exception->getMessage()}");
             return response()->json(DisplayResponse::reFailed($exception->getMessage()),500);
         }
     }
@@ -46,6 +49,7 @@ class AdvertiseCampaignController extends Controller
             $campaigns = (new EditCampaign())->run($request,$id);
             return new CampaignResource($campaigns);
         }catch (Exception $exception){
+            Log::error("Update Campaign error : {$exception->getMessage()}");
             return response()->json(DisplayResponse::reFailed($exception->getMessage()),500);
         }
     }
